54e1cfa0a6f3ffdf2eb4e28889d716483436f98e
Eliseo Joaquín Romero <78494234+EliseoRom@users.noreply.github.com>
2021-11-01 15:51:59 -0300

    Componentes

---
+++ Frontend/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+
+export default function Home() {
+  return (
+    <div >
+        <Navbar/>
+        <p>Hola este es el home</p>
+          
+    </div>
+  );
+}
---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Navbar.module.css";
+
+
+export default function NavBar() {
+  return (
+    <div className={s.navBar}>
+      <ul className={s.ulNav}>
+        
+          <NavLink className={s.text} exact to="/">
+            About
+          </NavLink>
+          <NavLink className={s.text} exact to="/">
+            Home
+          </NavLink>
+          <NavLink className={s.text} exact to="/">
+           Mi perfil
+          </NavLink>
+        
+    
+      </ul>
+    </div>
+  );
+}
+
